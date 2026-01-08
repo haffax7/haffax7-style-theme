2e601412a450118565fa108fd43fb3986667b0ef
dooboo <42453432+haffax7@users.noreply.github.com>
2026-01-09 03:05:43 +0900

    Update haffax7-sidebar.js

--- javascripts/discourse/connectors/below-site-header/haffax7-sidebar.js
+++ javascripts/discourse/connectors/below-site-header/haffax7-sidebar.js
@@ -2,6 +2,6 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
   setupComponent(args, component) {
-    // 여기에 나중에 최근 댓글 API 호출 로직을 넣을 예정입니다.
+    // 최근 댓글 데이터를 가져오는 로직이 들어갈 자리입니다.
   },
 };
